6e7af25e33cde177bbedeb8c9ea726220d22a72a
Julian Kim <kimjulian@grinnell.edu>
2024-11-17 17:18:01 +0100

    Added skeleton composables for screens

---
+++ app/src/main/java/com/ait/finalproject/ui/screen/createpost/CreatePostScreen.kt
@@ -0,0 +1,8 @@
+package com.ait.finalproject.ui.screen.createpost
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CreatePostScreen() {
+
+}
---
+++ app/src/main/java/com/ait/finalproject/ui/screen/discover/DiscoverScreen.kt
@@ -0,0 +1,8 @@
+package com.ait.finalproject.ui.screen.discover
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DiscoverScreen() {
+
+}
