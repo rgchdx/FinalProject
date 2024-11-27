c1a00414c2f01604c97d57d5a14b9ec7fc0a55b2
Julian Kim <kimjulian@grinnell.edu>
2024-11-27 09:21:07 +0100

    Fixed Gradle build by updating SDKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.ait.finalproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ait.finalproject"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
