14a8588a231b87d9274f3f536595a2b784e263ae
Julian Kim <kimjulian@grinnell.edu>
2024-12-06 14:24:24 +0100

    Made create post screen UI better

--- app/src/main/java/com/ait/finalproject/ui/screen/createpost/CreatePostScreen.kt
+++ app/src/main/java/com/ait/finalproject/ui/screen/createpost/CreatePostScreen.kt
@@ -1,12 +1,12 @@
 package com.ait.finalproject.ui.screen.createpost
 
-import android.media.MediaMetadataRetriever
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -16,11 +16,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.ait.finalproject.R
 
@@ -61,7 +65,9 @@ fun CreatePostScreen() {
     )
      */
     Column(
-        modifier = Modifier.padding(20.dp)
+        modifier = Modifier
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         /*
         val videoUri = MediaMetadataRetriever().run{
@@ -69,6 +75,13 @@ fun CreatePostScreen() {
             getFrameAtTime(0)
         }
          */
+        Text("Upload a new post!", style = TextStyle(
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        ))
+        Spacer(
+            modifier = Modifier.height(8.dp)
+        )
         OutlinedTextField(value = title,
             modifier = Modifier.fillMaxWidth(),
             label = { Text(text = stringResource(R.string.post_title)) },
@@ -92,6 +105,10 @@ fun CreatePostScreen() {
                     .weight(1.0f)
                     .fillMaxWidth(),
                 contentScale = ContentScale.Crop
+            )
+        } else {
+            Spacer(
+                modifier = Modifier.weight(1f)
             )
         }
         Button(
