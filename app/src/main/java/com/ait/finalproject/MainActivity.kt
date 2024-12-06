d3b1ad08ed0ece0a8376b0c6dc6406ac0bcd58ec
Julian Kim <kimjulian@grinnell.edu>
2024-12-06 14:00:36 +0100

    Added top app bar

--- app/src/main/java/com/ait/finalproject/MainActivity.kt
+++ app/src/main/java/com/ait/finalproject/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.ait.finalproject.ui.navigation.MainNavHost
 import com.ait.finalproject.ui.navigation.MainNavigationBar
 import com.ait.finalproject.ui.theme.FinalProjectTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,13 @@ class MainActivity : ComponentActivity() {
             FinalProjectTheme {
                 val navHostController = rememberNavController()
                 Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("Camelot")
+                            }
+                        )
+                    },
                     bottomBar = {
                         MainNavigationBar(navHostController)
                     }
